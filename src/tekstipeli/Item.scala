6f63b8ec53345010fd7e40a08d3511e413e93cb3
matsuli <nuts.hol@gmail.com>
2016-11-20 12:45:03 +0200

    Item contain update

--- src/tekstipeli/Item.scala
+++ src/tekstipeli/Item.scala
@@ -2,7 +2,11 @@ package tekstipeli
 
 class Item(val name: String, val description: String, val contain: Boolean) {
   
+  private val contains = Map[String, Item]()
+  
   def isOpenable = this.contain
+  
+  
   
   /** Returns a short textual representation of the item (its name, that is). */
   override def toString = this.name
