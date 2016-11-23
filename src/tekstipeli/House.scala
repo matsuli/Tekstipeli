3e29e30089c718d147a245e5c190a0e50b937be1
Scripti <leinohen@gmail.com>
2016-11-23 18:45:25 +0200

    something

--- src/tekstipeli/House.scala
+++ src/tekstipeli/House.scala
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package tekstipeli
 
 class House(val rooms: Map[String, Area], val player: Player) {
@@ -21,28 +20,4 @@ class House(val rooms: Map[String, Area], val player: Player) {
 
   }
   
-=======
-package tekstipeli
-
-class House(val rooms: Map[String, Area], val player: Player) {
-  
-  def distanceToItem(item: String): Int = {
-    var distance = 0
-    var currentArea = Vector(this.player.location)
-    for(currentRoom <- 0 until currentArea.size) {
-      if(currentArea(currentRoom).neighboringAreas.exists( _.contains(item) )) { 
-        return distance
-      } else {
-        distance += 1
-        currentArea = currentArea.flatMap( _.neighboringAreas )
-      }
-    }
-    distance
-  }
-  
-  def placeItems = {
-
-  }
-  
->>>>>>> origin/master
 }
